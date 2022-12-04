80e5502a39044d198328d6aef86cdf85ee9b0426
samar <elayedsamar@gmail.com>
2022-12-04 10:01:37 +0100

    test

---
+++ mavenproject1/src/main/java/tp_pgm/TP_PGM.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package tp_pgm;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+import java.io.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Paint;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.DefaultDrawingSupplier;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYBarPainter;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.data.statistics.HistogramDataset;
+
+/**
+ *
+ * @author ebous
+ */
+public class TP_PGM {
+
+    public static void main(String[] args) throws Exception {
+
+        FileInputStream f = new FileInputStream("C:\\ELBETTAL\\option info si\\MEDEV\\ImagesTestPGM\\baboon.pgm");
+        Scanner d = new Scanner(f);
+        d.nextLine();  //first line contains P2
+        d.nextLine(); // second line contains #
+        String line = d.nextLine();  //third line contains height and width
+        Scanner s = new Scanner(line);
+        int width = s.nextInt();
+        int height = s.nextInt();
+        line = d.nextLine();  //forth line contains maxVal
+        s = new Scanner(line);
+        int maxVal = s.nextInt();
+        int[][] image = new int[height][width];
+        System.out.println(maxVal);
+        for (int i = 0; i < width ; i++) {
+            for (int j = 0; j < height; j++) {
+                image[i][j] = d.nextInt();
+            }
+        }
+
+        d = new Scanner(System.in);
+
+        System.out.print("Enter the file name: ");
+
+        String fileName = d.nextLine();
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            PrintStream ps = new PrintStream(fos);// Write the .pgm header (P2, width height, maxVal)
+            ps.println("P2");
+            ps.println("#");
+            ps.println( height+ " "  + width );
+            ps.println(maxVal);
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width ; j++) {
+                    ps.print(image[i][j]);
+                    // Separate numbers by space
+                    ps.print(" ");
+                }
+                // Make each image line a separate text line for
+                // easier viewing in a text editor
+                ps.println();
+            }
+
+            ps.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public class Histogram {
+
+    private static final int BINS = 256;
+    private final BufferedImage image = getImage();
+    private HistogramDataset dataset;
+    private XYBarRenderer renderer;
+
+    private BufferedImage getImage() {
+        try {
+            return ImageIO.read(new URL(
+                "https://i.imgur.com/kxXhIH1.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        return null;
+    }
+
+    private ChartPanel createChartPanel() {
+        // dataset
+        dataset = new HistogramDataset();
+        Raster raster = image.getRaster();
+        final int w = image.getWidth();
+        final int h = image.getHeight();
+        double[] r = new double[w * h];
+        r = raster.getSamples(0, 0, w, h, 0, r);
+        dataset.addSeries("Red", r, BINS);
+        r = raster.getSamples(0, 0, w, h, 1, r);
+        dataset.addSeries("Green", r, BINS);
+        r = raster.getSamples(0, 0, w, h, 2, r);
+        dataset.addSeries("Blue", r, BINS);
+        // chart
+        JFreeChart chart = ChartFactory.createHistogram("Histogram", "Value",
+            "Count", dataset, PlotOrientation.VERTICAL, true, true, false);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        renderer = (XYBarRenderer) plot.getRenderer();
+        renderer.setBarPainter(new StandardXYBarPainter());
+        // translucent red, green & blue
+        Paint[] paintArray = {
+            new Color(0x80ff0000, true),
+            new Color(0x8000ff00, true),
+            new Color(0x800000ff, true)
+        };
+        plot.setDrawingSupplier(new DefaultDrawingSupplier(
+            paintArray,
+            DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_STROKE_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE));
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setMouseWheelEnabled(true);
+        return panel;
+    }
+
+    private JPanel createControlPanel() {
+        JPanel panel = new JPanel();
+        panel.add(new JCheckBox(new VisibleAction(0)));
+        panel.add(new JCheckBox(new VisibleAction(1)));
+        panel.add(new JCheckBox(new VisibleAction(2)));
+        return panel;
+    }
+
+    private class VisibleAction extends AbstractAction {
+
+        private final int i;
+
+        public VisibleAction(int i) {
+            this.i = i;
+            this.putValue(NAME, (String) dataset.getSeriesKey(i));
+            this.putValue(SELECTED_KEY, true);
+            renderer.setSeriesVisible(i, true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            renderer.setSeriesVisible(i, !renderer.getSeriesVisible(i));
+        }
+    }
+
+    private void display() {
+        JFrame f = new JFrame("Histogram");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(createChartPanel());
+        f.add(createControlPanel(), BorderLayout.SOUTH);
+        f.add(new JLabel(new ImageIcon(image)), BorderLayout.WEST);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            new Histogram().display();
+        });
+    }
+}
+}
