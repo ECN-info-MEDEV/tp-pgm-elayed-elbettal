aa8e432439ff3116a32371c1ada5f85ed72c9298
ELBETTALO <eb.oussama3@gmail.com>
2023-01-26 12:15:03 +0100

    avancement pour la classe decodeur

---
+++ Downloads/tp-note-grp16-elbettal-elayed-main/tp-note-grp16-elbettal-elayed-main/src/main/java/com/mycompany/tp/note/medev/Codeur.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+package com.mycompany.tp.note.medev;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * classe definit le tour de jeu du codeur ou il choisit la combinaison de ses pions cachés
+ * @author samar
+ */
+public class Codeur extends Joueur {
+    /**
+     * liste des pions
+     */
+    private ArrayList<Pion> ListPion = new ArrayList<>();
+
+    /**
+     *constructeur
+     */
+    public Codeur() {
+        super() ;
+    }
+     
+    /**
+     * methode assure au codeur de choisir la combinaison de ses pions
+     */
+    public void choix() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Codeur : choisit 4 pions: J pour Jaune, R pour Rouge, B pour Bleu, V pour Vert");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i+1  + " ");
+            String str = sc.nextLine();
+
+            Pion P = new Pion(str);
+            ListPion.add(P); 
+            
+
+        }
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Pion> getListPion() {
+        return ListPion;
+    }
+
+
+    
+}
