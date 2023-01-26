9b3465b186cc99bbec61c8ab418bbbf1994b4a0d
ELBETTALO <eb.oussama3@gmail.com>
2023-01-26 12:16:27 +0100

    avancement pour la classe decodeur

---
+++ Downloads/tp-note-grp16-elbettal-elayed-main/tp-note-grp16-elbettal-elayed-main/src/main/java/com/mycompany/tp/note/medev/TPNoteMedev.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp.note.medev;
+
+/**
+ *
+ * @author samar
+ */
+public class TPNoteMedev {
+
+    /**
+     * tester le jeu
+     * @param args
+     */
+    public static void main(String[] args) {
+        PlateauJeu PJ = new PlateauJeu() ;
+        PJ.Manche();
+        
+    }
+}
